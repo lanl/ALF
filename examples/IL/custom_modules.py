d4f5fe84695f8653b2b7a79c7f3ef7e898e6c241
Ben Nebgen <bnebgen@lanl.gov>
2023-05-03 13:41:34 -0600

    All executors need to be present for job

--- examples/IL/custom_modules.py
+++ examples/IL/custom_modules.py
@@ -214,6 +214,44 @@ config_debug = Config(
             ),
         ),
         HighThroughputExecutor(
+            label='alf_QM_standby_executor',
+
+            # Optional: the network interface on the login node to
+            # which compute nodes can communicate
+            #address=address_by_interface('bond0.144'),
+            max_workers=1,
+            #cpu_affinity='alternating',
+
+
+            provider=SlurmProvider(
+                # Partition / QOS
+                #'regular',
+                #'ml4chem',
+                'debug',
+                init_blocks = 0,
+                min_blocks = 0,
+                max_blocks = 1,
+
+                nodes_per_block=1,
+                #workers_per_node=1,
+
+                # string to prepend to #SBATCH blocks in the submit
+                scheduler_options='#SBATCH --ntasks-per-node=36 --nodes=4 -A w23_ml4chem --qos debug --reservation debug',
+
+                # Command to be run before starting a worker
+                #worker_init=
+
+                # We request all hyperthreads on a node.
+                #launcher=SrunLauncher(overrides='-c 64'),
+                launcher=SimpleLauncher(),
+                walltime='2:00:00',
+
+                # Slurm scheduler on Cori can be slow at times,
+                # increase the command timeouts
+                cmd_timeout=30,
+            ),
+        ),
+        HighThroughputExecutor(
             label='alf_ML_executor',
 
             # Optional: the network interface on the login node to

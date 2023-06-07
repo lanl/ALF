bf914408146a5d4c3e761390da2839c8aa3e4807
Ben Nebgen <bnebgen@lanl.gov>
2023-06-07 13:34:48 -0600

    fis

--- alframework/__main__.py
+++ alframework/__main__.py
@@ -145,7 +145,7 @@ if '--test_builder' in sys.argv[2:] or '--test_sampler' in sys.argv[2:] or '--te
     builder_configuration = builder_task_queue.task_list[0].result()
     queue_output = builder_task_queue.get_task_results()
     test_configuration = queue_output[0][0]
-    if not system_checker(test_configuration,raise_on_fail=False,print_error=False):
+    if not system_checker(test_configuration,kill_on_fail=False,print_error=False):
         system_checker(test_configuration[0])
         test_configuration = test_configuration[0]
     print("Builder testing returned:")

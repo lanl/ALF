e695a76e50918b5a94d363f84068d8e4785ef4ba
Filipp Nikitin <filipp.nikitin.96@gmail.com>
2023-06-30 10:46:24 -0600

    create reduction file + numpy 1d intersection

--- alframework/tools/database.py
+++ alframework/tools/database.py
@@ -10,7 +10,7 @@ class Database:
         self.root = zarr.group(store=store, overwrite=allow_overwriting)
         if "data" not in self.root:
             self.root.create_group("data")
-            self.root["data"].array("database_size", np.array([0]))
+            self.root.array("database_size", np.array([0], dtype="int"))
             self.root.create_group("reductions")
         self.add_instance(data)
 
@@ -27,8 +27,8 @@ class Database:
             data = self.root["data"]
             if n_atoms not in data:
                 group = data.create_group(n_atoms)
-                placeholder = zarr.zeros((1,), chunks=(100,))
-                placeholder[0] = data["database_size"][0]
+                placeholder = zarr.zeros((1,), chunks=(100,), dtype="int")
+                placeholder[0] = self.root["database_size"][0]
                 group.array("indices", placeholder)
                 for key, value in item.items():
                     placeholder = zarr.zeros((1, *item[key].shape), chunks=(100,))
@@ -36,32 +36,29 @@ class Database:
                     group.array(key, placeholder)
             else:
                 group = data[n_atoms]
-                group["indices"].append(data["database_size"])
+                group["indices"].append(self.root["database_size"])
                 for key, value in item.items():
                     group[key].append(value.reshape(1, *value.shape))
-            data["database_size"][0] += 1
+            self.root["database_size"][0] += 1
 
         else:
             raise NotImplementedError
 
     def get_item(self, selection_index):
         if isinstance(selection_index, list):
-            selection_index = set(selection_index)
+            selection_index = selection_index
             results = []
 
             data = self.root["data"]
             for key in data:
-                if key != "database_size":
-                    group_index = data[key]["indices"][:]
-                    group_index_set = set(group_index)
-                    index = group_index_set & selection_index
-                    for idx in index:
-                        position = np.where(group_index == idx)[0]
-                        data_dict = {}
-                        group = data[key]
-                        for inner_key in group:
-                            data_dict[inner_key] = group[inner_key][position]
-                    results.append(data_dict)
+                group_index = data[key]["indices"][:]
+                _, positions, _ = np.intersect1d(group_index, selection_index, return_indices=True)
+                for pos in positions:
+                    data_dict = {}
+                    group = data[key]
+                    for inner_key in group:
+                        data_dict[inner_key] = group[inner_key][pos]
+                results.append(data_dict)
             return results
 
         elif isinstance(selection_index, int):
@@ -69,8 +66,23 @@ class Database:
         else:
             raise NotImplementedError
 
-    def create_reduction(self, name, fraction):
-        pass
+    def create_reduction(self, name, fraction, overwrite=False):
+        database_size = self.root["database_size"][0]
+        subset_size = int(fraction * database_size)
+        initial_subset_indices = np.random.choice(np.arange(database_size), subset_size)
+        self.root["reductions"].create_group(name, overwrite=overwrite)
+        self.root[f"reductions/{name}"].create_group("0")
+        for group in self.root["data/"]:
+            self.root[f"reductions/{name}/0/"].create_group(group)
+            pointer = self.root[f"reductions/{name}/0/{group}"]
+            pointer.array("indices", self.root[f"data/{group}/indices"])
+            indices = pointer["indices"][:]
+            mask = np.zeros_like(indices)
+            _, positions, _ = np.intersect1d(indices, initial_subset_indices, return_indices=True)
+            mask[positions] = 1
+            placeholder = zarr.zeros(indices.shape, chunks=(100,), dtype="int")
+            placeholder[:] = mask
+            pointer.array("mask", placeholder)
 
     def update_reduction(self, name):
         pass
@@ -103,7 +115,11 @@ if __name__ == "__main__":
         database.add_instance(another_item)
 
     database = Database.load_from_zarr("./example_data")
-    pprint(database.root["data"]["database_size"][0])
+    pprint(database.root["database_size"][0])
 
     pprint(database.get_item([0, 10, 1001])[0])
     pprint(database.get_item(1001))
+
+    print(np.intersect1d([1, 3, 4, 3], np.array([3, 1, 2, 1]), return_indices=True))
+
+    database.create_reduction("lol", 0.1)

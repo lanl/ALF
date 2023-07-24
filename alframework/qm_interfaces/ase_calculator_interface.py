d4b93185df519a157f230b7200bc94aecfbe96ac
vitorgrizzi <66341481+vitorgrizzi@users.noreply.github.com>
2023-07-24 11:19:26 -0500

    Update ase_calculator_interface.py

--- alframework/qm_interfaces/ase_calculator_interface.py
+++ alframework/qm_interfaces/ase_calculator_interface.py
@@ -2,7 +2,6 @@ import os
 import json
 import pickle
 import re
-
 import parsl
 from parsl import python_app, bash_app
 
@@ -10,29 +9,32 @@ import os
 import ase
 from ase import Atoms
 from ase.io import vasp as vasp_io
-from alframework.tools.tools import system_checker
+
 from alframework.tools.tools import load_module_from_config
+from alframework.tools.molecules_class import MoleculesObject
+
 
 @python_app(executors=['alf_QM_executor'])
 def ase_calculator_task(molecule_object, QM_config, QM_scratch_dir, properties_list):
     """Creates an ASE calculator based on the parameters in qm_config.json.
 
     Args:
-        molecule_object (MoleculeObject): An object from the class MoleculeObject.
-        QM_config (str): json file containing the parameters needed to run the QM code.
+        molecule_object (MoleculesObject): An object from the class MoleculesObject.
+        QM_config (dict): json file containing the parameters needed to run the QM code.
         QM_scratch_dir (str): Directory to store the QM calculations.
         properties_list (dict): Dict whose keys are the properties that we would like ASE to extract from the QM
                                 calculation output file.
     Returns:
-        (list): A modified molecule_object that now stores the results of the QM calculations in molecules_object[2].
+        (MoleculesObject): A MoleculesObject containing the results of the QM calculations.
 
     """
-    system_checker(molecule_object)
-    directory = QM_scratch_dir + '/' + molecule_object[0]['moleculeid']
+    assert isinstance(molecule_object, MoleculesObject), 'molecule_object must be an instance of MoleculesObject'
+
+    directory = QM_scratch_dir + '/' + molecule_object.get_moleculeid()
     command = QM_config['QM_run_command']
     properties = list(properties_list.keys())
-    atoms = molecule_object[1]
-    
+    atoms = molecule_object.get_atoms()
+
     if os.path.isdir(directory):
         raise RuntimeError('Scratch directory exists: ' + directory)
     else:
@@ -47,14 +49,12 @@ def ase_calculator_task(molecule_object, QM_config, QM_scratch_dir, properties_l
     
     # Run the calculation
     calc.calculate(atoms=atoms, properties=properties)
+
+    # Stores results
+    molecule_object.store_results(calc.results)
+    molecule_object.set_converged_flag(calc.converged)
     
-    return_results = calc.results
-    return_results['converged'] = calc.converged
-    
-    return_system = [molecule_object[0], molecule_object[1], return_results]
-    system_checker(return_system)
-    
-    return return_system
+    return molecule_object
 
    
 @python_app(executors=['alf_QM_executor'])
@@ -62,43 +62,41 @@ def VASP_ase_calculator_task(molecule_object, QM_config, QM_scratch_dir, propert
     """ASE VASP calculator based on the parameters in qm_config.json.
 
     Args:
-        molecule_object(list): A list [metadata_dict, ase.Atoms, QM_results_dict] that uniquely represents a system.
-                               The first element is a dictionary contaning metadata (one of which must be 'moleculeid'),
-                               the second is an ase.Atoms object, and the third an empty dictionary that will
-                               store the results of the QM calculation.
+        molecule_object (MoleculesObject): An object from the class MoleculesObject.
         QM_config (str): Relative path of the QM_config.json file that contains the parameters needed to run VASP.
         QM_scratch_dir (str): Relative path of the directory to store the QM calculations.
         properties_list (dict): Dict whose keys are the properties that we would like ASE to extract from the OUTCAR
                                 file and that will be stored in the database.
 
     Returns:
-        (list): A modified molecule_object that now stores the results of the QM calculations as a dictionary in
-                molecule_object[2].
+        (MoleculesObject): A MoleculesObject containing the results of the QM calculations.
 
     """
     from ase.calculators.vasp import Vasp as calc_class
 
+    assert isinstance(molecule_object, MoleculesObject), 'molecule_object must be an instance of MoleculesObject'
+
     command = QM_config['QM_run_command']
-    directory = QM_scratch_dir + '/' + molecule_object[0]['moleculeid']
+    directory = QM_scratch_dir + '/' + molecule_object.get_moleculeid()
     properties = list(properties_list.keys())
 
     # Define the calculator
     calc_input = QM_config['input'].copy()
-    if 'kpoints' in molecule_object[0]:
-        calc_input['kpts'] = molecule_object[0]['kpoints']
+    if 'kpoints' in molecule_object.metadata.keys(): # I don't think this is necessary, kpts should be defined in QM_config.
+        calc_input['kpts'] = molecule_object.metadata['kpoints']
     calc = calc_class(directory=directory, command=command, **calc_input)
     
     # Run the calculation
-    calc.calculate(atoms=molecule_object[1], properties=properties)
+    calc.calculate(atoms=molecule_object.get_atoms(), properties=properties)
 
-    molecule_object[2] = calc.results
+    molecule_object.store_results(calc.results)
 
     convergedre = re.compile('aborting loop because EDIFF is reached')
     txt = open(directory + '/OUTCAR','r').read()
     if len(convergedre.findall(txt)) == 1:
-        molecule_object[2]['converged'] = True
+        molecule_object.set_converged_flag(True)
     else:
-        molecule_object[2]['converged'] = False
+        molecule_object.set_converged_flag(False)
 
     return molecule_object
 

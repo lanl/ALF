7fed311478b6357ad86b3b0db9b41585d62f4202
Filipp Nikitin <filipp.nikitin.96@gmail.com>
2023-08-01 20:14:37 -0600

    add store to zarr function

--- alframework/tools/tools.py
+++ alframework/tools/tools.py
@@ -1,15 +1,17 @@
-import os
 import glob
-from importlib import import_module
-import numpy as np
-import json
-from ase.geometry import complete_cell
-from ase import Atoms
-from alframework.tools import pyanitools as pyt
-from alframework.tools.molecules_class import MoleculesObject
 import inspect
+import json
+import os
+from importlib import import_module
 
+import numpy as np
+from ase import Atoms
+from ase.geometry import complete_cell
+
+from alframework.tools import pyanitools as pyt
 from alframework.tools.database import Database as ZarrDatabase
+from alframework.tools.molecules_class import MoleculesObject
+
 
 def annealing_schedule(t, tmax, amp, per, srt, end):
     """Defines the overall temperature profile in the molecular dynamics simulation.
@@ -29,7 +31,7 @@ def annealing_schedule(t, tmax, amp, per, srt, end):
     linear = t / tmax
     linear_T = (1 - linear) * srt + linear * end
 
-    return amp * np.sin(np.pi * t / per)**2 + linear_T
+    return amp * np.sin(np.pi * t / per) ** 2 + linear_T
 
 
 def build_ANI_info(directory):
@@ -68,7 +70,7 @@ def compute_empirical_formula(S):
     uniques = np.unique(S, return_counts=True)
     arg_sort = np.argsort(uniques[0])
 
-    return "_".join([i+str(j).zfill(2) for i,j in zip(uniques[0][arg_sort], uniques[1][arg_sort])])
+    return "_".join([i + str(j).zfill(2) for i, j in zip(uniques[0][arg_sort], uniques[1][arg_sort])])
 
 
 def random_rotation_matrix(deflection=1.0, randnums=None):
@@ -105,13 +107,87 @@ def random_rotation_matrix(deflection=1.0, randnums=None):
     return M
 
 
-def store_zarr_data(zarr_path, system_data, properties):
-    if os.path.exists(zarr_path):
-        zarr_database = ZarrDatabase.load_from_zarr(zarr_path)
-    pass
+def store_to_zarr(zarr_path, system_data, properties):
+    """Stores the key results of the QM calculations in the database.
 
+    Args:
+        zarr_path (str): Path to zarr database.
+        system_data (list): A list of MoleculesObjects objects.
+        properties (dict): Dictionary defined in master.json whose keys are the properties that we want to retrieve
+                           from the QM calculations and store in the database.
 
+    Returns:
+        (None)
 
+    """
+    # system data is a list of [mol-id(string), atoms, properties dictionary]
+    data_dict = {}
+    print("\n Saving h5 file: " + zarr_path)
+    total_number = len(system_data)
+    saved_number = 0
+    nan_number = 0
+    unconverged_number = 0
+    for system in system_data:
+        assert isinstance(system, MoleculesObject), 'system must be an instance of MoleculesObject'
+
+        cur_moliculeid = system.get_moleculeid()
+        cur_atoms = system.get_atoms()
+        cur_properties = system.get_results()
+        molkey = compute_empirical_formula(cur_atoms.get_chemical_symbols())
+
+        # Ensure system converged before saving
+        if system.check_convergence():
+            saved_number += 1
+            atom_index = np.argsort(cur_atoms.get_atomic_numbers())
+            # If there is already a molecule with the same formula, append
+            if molkey in data_dict:
+                data_dict[molkey]["_id"].append(cur_moliculeid)
+                data_dict[molkey]["coordinates"].append(cur_atoms.get_positions()[atom_index])
+                if any(cur_atoms.get_pbc()):
+                    data_dict[molkey]["cell"].append(complete_cell(cur_atoms.get_cell()))
+                for prop in properties:
+                    if properties[prop][1].lower() == "system":
+                        data_dict[molkey][properties[prop][0]].append(cur_properties[prop] * properties[prop][2])
+                    elif properties[prop][1].lower() == "atomic":
+                        data_dict[molkey][properties[prop][0]].append(
+                            np.array(cur_properties[prop])[atom_index] * properties[prop][2])
+                    else:
+                        raise RuntimeError('Unknown property format')
+            # If there is not already a molecule with this empirical formula, make a new one
+            else:
+                data_dict[molkey] = {}
+                data_dict[molkey]["species"] = np.array(cur_atoms.get_chemical_symbols())[atom_index]
+                data_dict[molkey]["_id"] = [cur_moliculeid]
+                data_dict[molkey]["coordinates"] = [cur_atoms.get_positions()[atom_index]]
+                if any(cur_atoms.get_pbc()):
+                    data_dict[molkey]["cell"] = [complete_cell(cur_atoms.get_cell())]
+                for prop in properties.keys():
+                    if properties[prop][1].lower() == "system":
+                        data_dict[molkey][properties[prop][0]] = [cur_properties[prop] * properties[prop][2]]
+                    elif properties[prop][1].lower() == "atomic":
+                        data_dict[molkey][properties[prop][0]] = [
+                            np.array(cur_properties[prop])[atom_index] * properties[prop][2]]
+                    else:
+                        raise RuntimeError('Unknown property format')
+        elif not isinstance(system,
+                            MoleculesObject):  # code never enter in this line, but leaving for now to avoid problems
+            nan_number += 1
+        elif not system.check_convergence():
+            unconverged_number = unconverged_number + 1
+        else:
+            print("Warning: molecule not saved for unknown reason")
+    print("Total Systems: " + str(total_number))
+    print("Saved Systems: " + str(saved_number))
+    print("NAN Systems: " + str(nan_number))
+    print("Unconverged Systems: " + str(unconverged_number))
+
+    if len(data_dict) > 0:
+        data_list = [v for k, v in data_dict.items()]
+        if os.path.exists(zarr_path):
+            database = ZarrDatabase.load_from_zarr(zarr_path)
+        else:
+            database = ZarrDatabase(zarr_path)
+        database.add_instance(data_list)
 
 
 def store_current_data(h5path, system_data, properties):
@@ -128,7 +204,6 @@ def store_current_data(h5path, system_data, properties):
 
     """
     # system data is a list of [mol-id(string), atoms, properties dictionary]
-    print(system_data)
     data_dict = {}
     print("Saving h5 file: " + h5path)
     total_number = len(system_data)
@@ -142,7 +217,7 @@ def store_current_data(h5path, system_data, properties):
         cur_atoms = system.get_atoms()
         cur_properties = system.get_results()
         molkey = compute_empirical_formula(cur_atoms.get_chemical_symbols())
-        
+
         # Ensure system converged before saving
         if system.check_convergence():
             saved_number += 1
@@ -157,7 +232,8 @@ def store_current_data(h5path, system_data, properties):
                     if properties[prop][1].lower() == "system":
                         data_dict[molkey][properties[prop][0]].append(cur_properties[prop] * properties[prop][2])
                     elif properties[prop][1].lower() == "atomic":
-                        data_dict[molkey][properties[prop][0]].append(np.array(cur_properties[prop])[atom_index] * properties[prop][2])
+                        data_dict[molkey][properties[prop][0]].append(
+                            np.array(cur_properties[prop])[atom_index] * properties[prop][2])
                     else:
                         raise RuntimeError('Unknown property format')
             # If there is not already a molecule with this empirical formula, make a new one
@@ -172,10 +248,12 @@ def store_current_data(h5path, system_data, properties):
                     if properties[prop][1].lower() == "system":
                         data_dict[molkey][properties[prop][0]] = [cur_properties[prop] * properties[prop][2]]
                     elif properties[prop][1].lower() == "atomic":
-                        data_dict[molkey][properties[prop][0]] = [np.array(cur_properties[prop])[atom_index] * properties[prop][2]]
+                        data_dict[molkey][properties[prop][0]] = [
+                            np.array(cur_properties[prop])[atom_index] * properties[prop][2]]
                     else:
                         raise RuntimeError('Unknown property format')
-        elif not isinstance(system, MoleculesObject): # code never enter in this line, but leaving for now to avoid problems
+        elif not isinstance(system,
+                            MoleculesObject):  # code never enter in this line, but leaving for now to avoid problems
             nan_number += 1
         elif not system.check_convergence():
             unconverged_number = unconverged_number + 1
@@ -186,23 +264,21 @@ def store_current_data(h5path, system_data, properties):
     print("NAN Systems: " + str(nan_number))
     print("Unconverged Systems: " + str(unconverged_number))
 
-    print(data_dict)
-
     for isokey in data_dict:
         # print('isokeys:',isokey)
         for propkey in data_dict[isokey]:
-            if propkey.lower() in ['species','_id']:
+            if propkey.lower() in ['species', '_id']:
                 data_dict[isokey][propkey] = [el.encode('utf-8') for el in list(data_dict[isokey][propkey])]
                 data_dict[isokey][propkey] = np.array(data_dict[isokey][propkey])
             else:
                 data_dict[isokey][propkey] = np.array(data_dict[isokey][propkey])
                 # print("encoding species")
-#                    if type(data_dict[isokey][propkey]) is 'numpy.ndarray':
-#                        data_dict[isokey][propkey] = np.stack(data_dict[isokey][propkey])
-#                    else:
-#                        data_dict[isokey][propkey] = np.array(data_dict[isokey][propkey])
-#                    print('propkey:', propkey,data_dict[isokey][propkey].shape)
-#
+    #                    if type(data_dict[isokey][propkey]) is 'numpy.ndarray':
+    #                        data_dict[isokey][propkey] = np.stack(data_dict[isokey][propkey])
+    #                    else:
+    #                        data_dict[isokey][propkey] = np.array(data_dict[isokey][propkey])
+    #                    print('propkey:', propkey,data_dict[isokey][propkey].shape)
+    #
     dpack = pyt.datapacker(h5path)
     for key in data_dict:
         dpack.store_data(key, **data_dict[key])
@@ -214,35 +290,35 @@ class parsl_task_queue():
     def __init__(self):
         # Create a list
         self.task_list = []
-    
-    def add_task(self,task):
+
+    def add_task(self, task):
         """Add a task to the task list
         """
         self.task_list.append(task)
-        #self.task_list[-1].start()
-        
+        # self.task_list[-1].start()
+
     def get_completed_number(self):
         """Get the number of completed tasks.
         """
         task_status = [task.done() for task in self.task_list]
         return int(np.sum(task_status))
-        
+
     def get_running_number(self):
         """Get the number of running tasks.
         """
         task_status = [task.running() for task in self.task_list]
         return int(np.sum(task_status))
-        
+
     def get_number(self):
         """Get the the number of tasks in the task list.
         """
         return len(self.task_list)
-    
+
     def get_queued_number(self):
         """Get the number of queued tasks.
         """
         return int(self.get_number() - self.get_running_number() - self.get_completed_number())
-            
+
     def get_task_results(self):
         """Get the reults of the tasks
 
@@ -262,7 +338,7 @@ class parsl_task_queue():
                 del self.task_list[taski]
 
         return results_list, failed_number
-    
+
     def get_task_status(self):
         """Get the status of the tasks.
 
@@ -274,7 +350,7 @@ class parsl_task_queue():
             status_list.append(task.task_status())
 
         return status_list
-        
+
     def print_status(self):
         """Prints the status of the tasks.
         """
@@ -321,14 +397,14 @@ def system_checker(system, kill_on_fail=True, print_error=True):
         (bool): True if 'system' meets all requirements and False otherwise.
 
     """
-    try: 
+    try:
         assert isinstance(system, list) or isinstance(system, tuple)
         assert len(system) == 3
         assert isinstance(system[0], dict)
         assert isinstance(system[0]['moleculeid'], str)
         assert isinstance(system[1], Atoms)
         assert isinstance(system[2], dict)
-        
+
         no_nan = True
         if np.sum(np.isnan(system[1].get_positions())) > 0:
             no_nan = False
@@ -341,9 +417,9 @@ def system_checker(system, kill_on_fail=True, print_error=True):
                     no_nan = False
         if not no_nan:
             raise RuntimeError('NAN in system')
-        
+
         return True
-        
+
     except Exception as e:
         if print_error:
             print(e)
@@ -368,8 +444,8 @@ def load_config_file(path, master_directory=None):
     if master_directory is None and "master_directory" in config:
         if config["master_directory"] == 'pwd':
             master_directory = os.getcwd() + '/'
-        else: 
-            master_directory = config["master_directory"] + '/' 
+        else:
+            master_directory = config["master_directory"] + '/'
         config["master_directory"] = master_directory
 
     if isinstance(config, dict):
@@ -381,7 +457,7 @@ def load_config_file(path, master_directory=None):
                 if config[entry][0] != '/':
                     config[entry] = master_directory + config[entry]
                 # For every 'path' entry, make a corresponding 'dir' entry that holds files in the path
-                dir_dict[entry[:-4]+'dir'] = '/'.join(config[entry].split('/')[:-1]) + '/'
+                dir_dict[entry[:-4] + 'dir'] = '/'.join(config[entry].split('/')[:-1]) + '/'
         config.update(dir_dict)
 
     return config
@@ -443,7 +519,7 @@ def build_input_dict(function, dictionary_list, use_local_space=False, raise_on_
             if parameter in cur_dict.keys():
                 return_dictionary[parameter] = cur_dict[parameter]
                 break
-        if not (parameter in return_dictionary): # Still have not found the variable
+        if not (parameter in return_dictionary):  # Still have not found the variable
             if use_local_space and (parameter in local_space_dict):
                 return_dictionary[parameter] = local_space_dict[parameter]
             elif sig.parameters[parameter].default != inspect._empty:
@@ -452,6 +528,7 @@ def build_input_dict(function, dictionary_list, use_local_space=False, raise_on_
             elif parameter == 'self':
                 pass
             elif raise_on_fail:
-                raise ValueError("Required input parameter {:s} of {:s} not defined in any space.".format(parameter, function.__name__))
+                raise ValueError("Required input parameter {:s} of {:s} not defined in any space.".format(parameter,
+                                                                                                          function.__name__))
 
     return return_dictionary

30a159d92b0e29950679f1e0efd93df6b21b2a84
Nebgen <315312@win.lanl.gov>
2026-08-24 12:31:40 -0600

    Fixed bug with n_atoms in hippynn_interface
    If configurations were removed due to 'dist_soft_min', error would
    result due to mis-matched length of n_atoms and energy_key.
    
    Changed ordering of n_atom compute and min_dists removal.

--- alframework/ml_interfaces/hippynn_interface.py
+++ alframework/ml_interfaces/hippynn_interface.py
@@ -375,18 +375,17 @@ def train_HIPNN_model(model_dir,
 
             arrays[species_key] = arrays[species_key].to(torch.int64)   # or .long()
 
+            min_dists = hippynn.pretraining.calculate_min_dists(database.arr_dict,species_key,coordinates_key,6.0,cell_name=cell_key,batch_size=256)
+            min_dists_flag = min_dists>network_params["dist_soft_min"]
+            for curK in database.arr_dict.keys():
+                database.arr_dict[curK] = database.arr_dict[curK][min_dists_flag]
+                
             n_atoms = (
               arrays[species_key]          # tensor
               .to(torch.bool)              # cast to bool
               .to(torch.int32)             # or .int() / .to(torch.int64) as you prefer
               .sum(dim=1)                  # sum along atom axis
              )
-            
-            
-            min_dists = hippynn.pretraining.calculate_min_dists(database.arr_dict,species_key,coordinates_key,6.0,cell_name=cell_key,batch_size=256)
-            min_dists_flag = min_dists>network_params["dist_soft_min"]
-            for curK in database.arr_dict.keys():
-                database.arr_dict[curK] = database.arr_dict[curK][min_dists_flag]
 
             if not( energy_key+"peratom" in list(arrays.keys())):
                 arrays[energy_key+"peratom"] =  arrays[energy_key] / n_atoms
